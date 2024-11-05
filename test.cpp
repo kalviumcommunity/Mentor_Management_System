ad63299bc8949c5f6d4ee640a5dc0afb405d7d1d
devasree A <devasree.a@kalvium.community>
2024-11-05 09:41:41 +0530

    solid principle 1

--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <string>
 
-// Base Class (Person) - Abstract Class
+// Base Class (Person) - Abstract Class, handles generic details of a person
 class Person {
 protected:
     std::string name;
@@ -27,7 +27,7 @@ public:
     bool isAvailable() const { return availability; }
 
     // Pure virtual method - makes Person an abstract class
-    virtual void checkIn() = 0;  // Must be implemented in derived classes
+    virtual void checkIn() = 0;
 
     // Virtual method for checking out a person
     virtual void checkOut() {
@@ -36,31 +36,13 @@ public:
     }
 };
 
-// Derived Class: Mentor
+// Mentor Class - Handles mentor-specific attributes and behaviors
 class Mentor : public Person {
 private:
     std::string type;  // Technical or Program
     std::string expertise;
 
-    static int mentorCount;
-    static std::vector<Mentor*> mentorList;
-
 public:
-    // Constructor
-    Mentor() {
-        ++mentorCount;
-        mentorList.push_back(this);
-    }
-
-    // Destructor
-    ~Mentor() {
-        --mentorCount;
-        auto it = std::find(mentorList.begin(), mentorList.end(), this);
-        if (it != mentorList.end()) {
-            mentorList.erase(it);
-        }
-    }
-
     // Accessor and Mutator Methods for Mentor-specific attributes
     std::string getType() const { return type; }
     void setType(const std::string& mentorType) { type = mentorType; }
@@ -73,52 +55,15 @@ public:
         availability = true;
         std::cout << name << " has checked in as Mentor." << std::endl;
     }
-
-    // Static function to display all mentors
-    static void displayMentors() {
-        std::cout << "List of Mentors:" << std::endl;
-        for (const auto& mentor : mentorList) {
-            std::cout << "Name: " << mentor->getName() << ", Type: " << mentor->getType() 
-                      << ", Expertise: " << mentor->getExpertise() 
-                      << ", Contact: " << mentor->getContactNumber() << std::endl;
-        }
-    }
-
-    // Static function to get the total number of mentors
-    static int getMentorCount() {
-        return mentorCount;
-    }
 };
 
-// Static member initialization for Mentor class
-int Mentor::mentorCount = 0;
-std::vector<Mentor*> Mentor::mentorList;
-
-// Derived Class: Mentee
+// Mentee Class - Handles mentee-specific attributes and behaviors
 class Mentee : public Person {
 private:
     std::string type;  // Technical or Program
     std::string expertise;
 
-    static int menteeCount;
-    static std::vector<Mentee*> menteeList;
-
 public:
-    // Constructor
-    Mentee() {
-        ++menteeCount;
-        menteeList.push_back(this);
-    }
-
-    // Destructor
-    ~Mentee() {
-        --menteeCount;
-        auto it = std::find(menteeList.begin(), menteeList.end(), this);
-        if (it != menteeList.end()) {
-            menteeList.erase(it);
-        }
-    }
-
     // Accessor and Mutator Methods for Mentee-specific attributes
     std::string getType() const { return type; }
     void setType(const std::string& menteeType) { type = menteeType; }
@@ -131,26 +76,85 @@ public:
         availability = true;
         std::cout << name << " has checked in as Mentee." << std::endl;
     }
+};
+
+// MentorManager Class - Manages mentor count and list
+class MentorManager {
+private:
+    static int mentorCount;
+    static std::vector<Mentor*> mentorList;
+
+public:
+    static void addMentor(Mentor* mentor) {
+        ++mentorCount;
+        mentorList.push_back(mentor);
+    }
+
+    static void removeMentor(Mentor* mentor) {
+        --mentorCount;
+        auto it = std::find(mentorList.begin(), mentorList.end(), mentor);
+        if (it != mentorList.end()) {
+            mentorList.erase(it);
+        }
+    }
+
+    // Static function to display all mentors
+    static void displayMentors() {
+        std::cout << "List of Mentors:" << std::endl;
+        for (const auto& mentor : mentorList) {
+            std::cout << "Name: " << mentor->getName() << ", Type: " << mentor->getType()
+                      << ", Expertise: " << mentor->getExpertise()
+                      << ", Contact: " << mentor->getContactNumber() << std::endl;
+        }
+    }
+
+    static int getMentorCount() {
+        return mentorCount;
+    }
+};
+
+// Static member initialization for MentorManager class
+int MentorManager::mentorCount = 0;
+std::vector<Mentor*> MentorManager::mentorList;
+
+// MenteeManager Class - Manages mentee count and list
+class MenteeManager {
+private:
+    static int menteeCount;
+    static std::vector<Mentee*> menteeList;
+
+public:
+    static void addMentee(Mentee* mentee) {
+        ++menteeCount;
+        menteeList.push_back(mentee);
+    }
+
+    static void removeMentee(Mentee* mentee) {
+        --menteeCount;
+        auto it = std::find(menteeList.begin(), menteeList.end(), mentee);
+        if (it != menteeList.end()) {
+            menteeList.erase(it);
+        }
+    }
 
     // Static function to display all mentees
     static void displayMentees() {
         std::cout << "List of Mentees:" << std::endl;
         for (const auto& mentee : menteeList) {
-            std::cout << "Name: " << mentee->getName() << ", Type: " << mentee->getType() 
-                      << ", Expertise: " << mentee->getExpertise() 
+            std::cout << "Name: " << mentee->getName() << ", Type: " << mentee->getType()
+                      << ", Expertise: " << mentee->getExpertise()
                       << ", Contact: " << mentee->getContactNumber() << std::endl;
         }
     }
 
-    // Static function to get the total number of mentees
     static int getMenteeCount() {
         return menteeCount;
     }
 };
 
-// Static member initialization for Mentee class
-int Mentee::menteeCount = 0;
-std::vector<Mentee*> Mentee::menteeList;
+// Static member initialization for MenteeManager class
+int MenteeManager::menteeCount = 0;
+std::vector<Mentee*> MenteeManager::menteeList;
 
 // Main function
 int main() {
@@ -158,8 +162,7 @@ int main() {
     std::string name, contact, type, expertise;
 
     std::cout << "Enter details for Mentor or Mentee\n";
-    
-    // Loop to handle multiple people input
+
     for (int i = 0; i < 2; ++i) {
         std::cout << "Enter 1 for Mentor, 2 for Mentee: ";
         std::cin >> personType;
@@ -178,29 +181,31 @@ int main() {
         std::getline(std::cin, expertise);
 
         if (personType == 1) {
-            // Create a Mentor
+            // Create and manage a Mentor
             Mentor* mentor = new Mentor();
             mentor->setName(name);
             mentor->setContactNumber(contact);
             mentor->setType(type);
             mentor->setExpertise(expertise);
             mentor->checkIn();
+            MentorManager::addMentor(mentor);
         } else if (personType == 2) {
-            // Create a Mentee
+            // Create and manage a Mentee
             Mentee* mentee = new Mentee();
             mentee->setName(name);
             mentee->setContactNumber(contact);
             mentee->setType(type);
             mentee->setExpertise(expertise);
             mentee->checkIn();
+            MenteeManager::addMentee(mentee);
         } else {
             std::cout << "Invalid option. Please enter either 1 or 2." << std::endl;
         }
     }
 
     // Display lists of Mentors and Mentees
-    Mentor::displayMentors();
-    Mentee::displayMentees();
+    MentorManager::displayMentors();
+    MenteeManager::displayMentees();
 
     return 0;
 }
